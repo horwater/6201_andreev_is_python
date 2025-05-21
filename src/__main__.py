f6c506153e355ff2dff5eb3ba8db4a728df2583d
Andreev <rafaelmarazuki@gmail.com>
2025-05-21 23:45:33 +0400

    lab5.2py cut 6

--- src/__main__.py
+++ src/__main__.py
@@ -2,12 +2,17 @@ import asyncio
 import sys
 from src.pypline import AsyncImagePipeline
 
-async def main(limit):
+def main():
+    if len(sys.argv) > 1:
+        limit = int(sys.argv[1])
+    else:
+        limit = 2
+    asyncio.run(run_main(limit))
+
+async def run_main(limit):
+    """Асинхронная часть пайплайна"""
     pipeline = AsyncImagePipeline(limit)
     await pipeline.run_pipeline()
 
-if (__name__ == '__main__'):
-    if len(sys.argv) > 1:
-        asyncio.run(main(int(sys.argv[1])))
-    else:
-        asyncio.run(main(4))
+if __name__ == '__main__':
+    main()
