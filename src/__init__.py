28e62d931ed4cd86d71bb90f29e17d1c32d62a75
Andreev <rafaelmarazuki@gmail.com>
2025-05-21 22:56:29 +0400

    lab5.2py cut 5

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from src.pypline import AsyncImagePipeline
+from src.image_processor import ImageProcessor
 
 print(f'Invoking __init__.py for {__name__}')
 __all__ = ['AsyncImagePipeline', 'ImageProcessor']
